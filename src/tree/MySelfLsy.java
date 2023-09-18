d10632ebf13cdf35d4a9e4ffac84cc64b728d45f
lsy28901 <tndus28901@gmail.com>
2023-09-18 19:53:41 +0900

    add MySelfLsy

--- src/tree/MySelfLsy.java
+++ src/tree/MySelfLsy.java
@@ -1,10 +1,7 @@
 package tree;
 
 public class MySelfLsy {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("이수연 입니다");
+		System.out.println("이수연입니다.");
 	}
-
 }
